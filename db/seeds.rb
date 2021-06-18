e5a8ffabc3de067c7d4ef5da673064a9bace5da3
Ananya <avagadi15@gmail.com>
2021-06-18 20:32:20 +0530

    adding entry via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "seed-gen user", description: "I also
+created this user without using the HTML form!")
