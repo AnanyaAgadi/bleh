2fdb177cffbc32775f1284376f1d388cd88428dc
Ananya <avagadi15@gmail.com>
2021-06-18 20:39:23 +0530

    adding new field & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_142151) do
+ActiveRecord::Schema.define(version: 2021_06_18_150614) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_142151) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "address"
   end
 
 end
