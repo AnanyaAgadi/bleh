3f3f0340afe8fe005af2d144df40b7271abe1f28
Ananya <avagadi15@gmail.com>
2021-06-19 12:11:53 +0530

    adding address to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :description, :image_url)
+      params.require(:user).permit(:name, :description, :image_url, :address)
     end
 end
