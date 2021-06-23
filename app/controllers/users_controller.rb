2b43c1b4ffc93b4b31ca6a15480b12a5f6d716f5
Ananya <avagadi15@gmail.com>
2021-06-23 15:47:48 +0530

    CF 4.3 cstm controller, views add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+    
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :orders, only: [:index, :show, :create, :destroy]
 	root 'welcome#landing_page'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
