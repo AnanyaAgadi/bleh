3a0860d27e8f12f4e43c7e349973740b6c093dfd
Ananya <avagadi15@gmail.com>
2021-06-25 16:49:57 +0530

    CF-4.5, adding img carsl & er fixing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    @users = User.limit(3)
   end
 
   def landing_page
-    @featured_user = User.first
+    # @featured_user = User.first
+    @users = User.limit(3)
   end
 end
